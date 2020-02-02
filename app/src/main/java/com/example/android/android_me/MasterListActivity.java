cdbbcf9e84a40e778b6c81df4f80a9bd14c1264a
Anand Balan <abalan@abalan-mac.local>
2020-02-02 13:43:01 -0800

    show a gridview of body parts in master list activity

---
+++ app/src/main/java/com/example/android/android_me/MasterListActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.android_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MasterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master_list);
+    }
+}
